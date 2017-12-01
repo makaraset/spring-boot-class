ff0cf062bdbf3fc56c62f1a1faba5b401c521abf
makeraset <makara.set@gmail.com>
2017-12-01 17:01:19 +0700

    SpringHw4

--- spring-boot-homework3/src/main/java/com/mkr/controller/BookController.java
+++ spring-boot-homework3/src/main/java/com/mkr/controller/BookController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mkr.model.Book;
 import com.mkr.service.BookService;
@@ -31,4 +34,37 @@ public class BookController {
 		
 		return "bookdetail";
 	}
+	
+	@GetMapping("/book/add")
+	private String boAddBook(Model model) {
+		model.addAttribute("actionstat", true);
+		model.addAttribute("book", new Book());
+		return "book";
+	}
+	
+	@GetMapping("/book/edit/")
+	private String boEditBook(Model model, @RequestParam("id") int id) {
+		model.addAttribute("actionstat", false);
+		model.addAttribute("book", bookService.findById(id));
+		return "book";
+	}
+	
+	@PostMapping("/book/add")
+	private String addBook(Book book) {
+		bookService.addBook(book);
+		return "redirect:/";
+	}
+	
+	@PostMapping("/book/edit")
+	private String updateBook(Book book) {
+		bookService.updateBook(book);
+		return "redirect:/";
+	}
+	
+	@PostMapping("/book/remove")
+	private String removeBook(@RequestParam("id") int id) {
+		bookService.removeBook(bookService.findById(id));
+		return "redirect:/";
+	}
+	
 }
--- spring-boot-homework3/src/main/java/com/mkr/model/Book.java
+++ spring-boot-homework3/src/main/java/com/mkr/model/Book.java
@@ -1,16 +1,15 @@
 package com.mkr.model;
 
-import java.util.Date;
 
 public class Book {
 	private int id;
 	private String title;
-	private Date publishDate;
+	private String publishDate;
 	private String author;
 	private int page;
 	private String coverImage;
 	
-	public Book(int id, String title, Date publishDate, String author, int page, String coverImage) {
+	public Book(int id, String title, String publishDate, String author, int page, String coverImage) {
 		super();
 		this.id = id;
 		this.title = title;
@@ -18,6 +17,10 @@ public class Book {
 		this.author = author;
 		this.page = page;
 		this.coverImage = coverImage;
+	}
+	
+	public Book(){
+		
 	}
 	
 	public int getId() {
@@ -32,10 +35,10 @@ public class Book {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public Date getPublishDate() {
+	public String getPublishDate() {
 		return publishDate;
 	}
-	public void setPublishDate(Date publishDate) {
+	public void setPublishDate(String publishDate) {
 		this.publishDate = publishDate;
 	}
 	public String getAuthor() {
